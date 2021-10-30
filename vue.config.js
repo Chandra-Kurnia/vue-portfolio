59d8d3abc72bdbbb9929b5c193523fa826d2f0e2
Chandra-Kurnia <chandrakqurniawan2002@gmail.com>
2021-10-30 13:44:54 +0700

    set up scss

---
+++ vue.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    css: {
+      loaderOptions: {
+        sass: {
+          additionalData: `
+            @import "@/scss/_variables.scss";
+            @import "@/scss/_mixins.scss";
+            @import "@/scss/_styles.scss";
+          `
+        }
+      }
+    }
+  };
